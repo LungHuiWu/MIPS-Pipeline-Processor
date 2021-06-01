b2ae23d4dabab7a3e79a6870a4d213189c19a01f
LungHuiWu <b07901019@ntu.edu.tw>
2021-06-01 18:28:20 +0800

    EX & MEM

--- MIPS/Baseline/src/CHIP.v
+++ MIPS/Baseline/src/CHIP.v
@@ -153,6 +153,7 @@ input  	[31:0] 	DCACHE_rdata;
 //--------- First Half -----------------------
 reg 	[31:0]	S1_PC, S1_PC_nxt;
 reg 	[31:0]	S1_inst, S1_inst_nxt;
+// WB = RegWrite + MemToReg
 reg 	[1:0] 	S2_WB, S2_WB_nxt;
 // M = Branch + MemRead + MemWrite
 reg		[2:0]	S2_M, S2_M_nxt;
@@ -179,9 +180,15 @@ reg 	[4:0]	S4_I, S4_I_nxt;
 
 //========= Wire ============================
 wire 	PCSrc;
+wire 	RegWrite;
+wire 	[31:0]	WriteData;
+wire 	[4:0]	WriteReg;
 
 //========= First Part ======================
 // ID
+assign	RegWrite = S4_WB[1];
+assign	WriteData = S4_WB[0] ? S4_rdata : S4_ALUResult;
+assign	WriteReg = S4_I;
 
 //========= Second Part =====================
 // EX
@@ -258,10 +265,456 @@ assign	DCACHE_wen = S3_M[0];
 assign	DCACHE_ren = S3_M[1];
 assign	PCSrc = S3_M[2] && S3_Zero;
 always @(*) begin
-	S4_rdata_nxt = DCACHE_rdata;
-	S4_WB_nxt = S3_WB;
-	S4_ALUResult_nxt = S3_ALUResult;
-	S4_I_nxt = S3_I;
+	S4_rdata_nxt = S4_rdata;
+	S4_WB_nxt = S4_WB;
+	S4_ALUResult_nxt = S4_ALUResult;
+	S4_I_nxt = S4_I;
+	if (!ICACHE_stall && !DCACHE_stall) begin
+		S4_rdata_nxt = DCACHE_rdata;
+		S4_WB_nxt = S3_WB;
+		S4_ALUResult_nxt = S3_ALUResult;
+		S4_I_nxt = S3_I;
+	end
+end
+//======== Sequetial Part =======================
+always @(posedge clk or negedge rst_n) begin
+	if(!rst_n)begin
+		S1_PC 			<= 0;
+		S1_inst 		<= 0;
+		S2_WB 			<= 0;
+		S2_M 			<= 0;
+		S2_EX 			<= 0;
+		S2_I1 			<= 0;
+		S2_I2 			<= 0;
+		S2_I3 			<= 0;
+		S2_rdata1 		<= 0;
+		S2_rdata2 		<= 0;
+		S2_PC 			<= 0;
+		S3_WB 			<= 0;
+		S3_M 			<= 0;
+		S3_Add 			<= 0;
+		S3_Zero 		<= 0;
+		S3_ALUResult 	<= 0;
+		S3_rdata 		<= 0;
+		S3_I 			<= 0;
+		S4_WB 			<= 0;
+		S4_rdata 		<= 0;
+		S4_ALUResult 	<= 0;
+		S4_I 			<= 0;
+	end
+	else begin
+		S1_PC 			<= S1_PC_nxt;
+		S1_inst 		<= S1_inst_nxt;
+		S2_WB 			<= S2_WB_nxt;
+		S2_M 			<= S2_M_nxt;
+		S2_EX 			<= S2_EX_nxt;
+		S2_I1 			<= S2_I1_nxt;
+		S2_I2 			<= S2_I2_nxt;
+		S2_I3 			<= S2_I3_nxt;
+		S2_rdata1 		<= S2_rdata1_nxt;
+		S2_rdata2 		<= S2_rdata2_nxt;
+		S2_PC 			<= S2_PC_nxt;
+		S3_WB 			<= S3_WB_nxt;
+		S3_M 			<= S3_M_nxt;
+		S3_Add 			<= S3_Add_nxt;
+		S3_Zero 		<= S3_Zero_nxt;
+		S3_ALUResult 	<= S3_ALUResult_nxt;
+		S3_rdata 		<= S3_rdata_nxt;
+		S3_I 			<= S3_I_nxt;
+		S4_WB 			<= S4_WB_nxt;
+		S4_rdata 		<= S4_rdata_nxt;
+		S4_ALUResult 	<= S4_ALUResult_nxt;
+		S4_I 			<= S4_I_nxt;
+	end
+end
+
+endmodule
+
+
+
+// Cache
+module cache(
+    clk,
+    proc_reset,
+    proc_read,
+    proc_write,
+    proc_addr,
+    proc_rdata,
+    proc_wdata,
+    proc_stall,
+    mem_read,
+    mem_write,
+    mem_addr,
+    mem_rdata,
+    mem_wdata,
+    mem_ready
+);
+    
+//==== input/output definition ============================
+    input          clk;
+    // processor interface
+    input          proc_reset;
+    input          proc_read, proc_write;
+    input   [29:0] proc_addr;
+    input   [31:0] proc_wdata;
+    output         proc_stall;
+    output  [31:0] proc_rdata;
+    // memory interface
+    input  [127:0] mem_rdata;
+    input          mem_ready;
+    output         mem_read, mem_write;
+    output  [27:0] mem_addr;
+    output [127:0] mem_wdata;
+
+//==== parameters =========================================
+
+    parameter WORDLEN = 32;
+    parameter BLOCKNUM = 4;
+    parameter TAGLEN = 26;
+
+    parameter NONE = 2'd0;
+    parameter ONE = 2'd1;
+    parameter TWO = 2'd2;
+
+    parameter IDLE = 3'd0;
+    parameter COMPARE = 3'd1;
+    parameter ALLOCATE = 3'd2;
+    parameter WRITEBACK = 3'd3;
+    parameter READ = 3'd4;
+    parameter WRITE = 3'd5;
+    
+//==== wire/reg definition ================================
+    
+    /// internal FF
+    // state
+    reg     [2:0]   state, state_nxt;
+    reg     [1:0]   set, set_nxt;
+    // cache 1
+    reg     [WORDLEN*4-1:0] cch1        [0:BLOCKNUM-1];
+    reg     [WORDLEN*4-1:0] cch1_nxt    [0:BLOCKNUM-1];
+    reg     [TAGLEN-1:0]    tag1        [0:BLOCKNUM-1];
+    reg     [TAGLEN-1:0]    tag1_nxt    [0:BLOCKNUM-1];
+    reg     valid1      [0:BLOCKNUM-1];
+    reg     valid1_nxt  [0:BLOCKNUM-1];
+    reg     dirty1      [0:BLOCKNUM-1];
+    reg     dirty1_nxt  [0:BLOCKNUM-1];
+    // cache 1
+    reg     [WORDLEN*4-1:0] cch2        [0:BLOCKNUM-1];
+    reg     [WORDLEN*4-1:0] cch2_nxt    [0:BLOCKNUM-1];
+    reg     [TAGLEN-1:0]    tag2        [0:BLOCKNUM-1];
+    reg     [TAGLEN-1:0]    tag2_nxt    [0:BLOCKNUM-1];
+    reg     valid2      [0:BLOCKNUM-1];
+    reg     valid2_nxt  [0:BLOCKNUM-1];
+    reg     dirty2      [0:BLOCKNUM-1];
+    reg     dirty2_nxt  [0:BLOCKNUM-1];
+
+    /// output FF
+    reg     proc_stall, proc_stall_nxt;
+    reg     [31:0]  proc_rdata, proc_rdata_nxt;
+    reg     mem_read, mem_read_nxt;
+    reg     mem_write, mem_write_nxt;
+    reg     [27:0]  mem_addr, mem_addr_nxt;
+    reg     [127:0] mem_wdata, mem_wdata_nxt;
+
+    wire    [1:0]   block_now;
+    wire    [25:0]  tag_now;
+    wire    [1:0]   word_idx;
+    wire    hit1;
+    wire    hit2;
+    wire    miss1_clean;
+    wire    miss1_dirty;
+    wire    miss2_clean;
+    wire    miss2_dirty;
+    wire    hit;
+    wire    miss;
+
+    integer i;
+
+//==== combinational circuit ==============================
+
+assign block_now = proc_addr[3:2];
+assign tag_now = proc_addr[29:4];
+assign word_idx = proc_addr[1:0];
+
+assign hit1 = (valid1[block_now]) && (tag1[block_now] == tag_now);
+assign hit2 = (valid2[block_now]) && (tag2[block_now] == tag_now);
+assign miss1_clean = ~hit1 && ~dirty1[block_now];
+assign miss2_clean = ~hit2 && ~dirty2[block_now];
+assign miss1_dirty = ~hit1 && dirty1[block_now];
+assign miss2_dirty = ~hit2 && dirty2[block_now];
+assign hit = hit1 || hit2;
+assign miss = ~hit;
+
+always @(*) begin // FSM
+    state_nxt = state;
+    set_nxt = NONE;
+    proc_stall_nxt = 0;
+    case (state)
+        IDLE: begin
+            //$display("idle");
+            if (proc_read || proc_write) begin
+                state_nxt = COMPARE;
+                proc_stall_nxt = 1;
+                set_nxt = NONE;
+            end
+            else begin
+                state_nxt = IDLE;
+                proc_stall_nxt = 0;
+                set_nxt = NONE;
+            end
+        end 
+        COMPARE: begin
+            //$display("compare");
+            proc_stall_nxt = 1;
+            if (hit) begin
+                if (proc_write && ~proc_read) begin
+                    state_nxt = WRITE;
+                end
+                else if (proc_read && ~proc_write) begin
+                    state_nxt = READ;
+                end
+                else state_nxt = IDLE;
+
+                if (hit1) begin
+                    set_nxt = ONE;
+                end
+                else if (hit2) begin
+                    set_nxt = TWO;
+                end
+                else set_nxt = NONE;
+            end
+            else begin
+                if (miss1_clean) begin
+                    state_nxt = ALLOCATE;
+                    set_nxt = ONE;
+                end
+                else if (miss1_dirty) begin
+                    state_nxt = WRITEBACK;
+                    set_nxt = ONE;
+                end
+                else if (miss2_clean) begin
+                    state_nxt = ALLOCATE;
+                    set_nxt = TWO;
+                end
+                else if (miss2_dirty) begin
+                    state_nxt = WRITEBACK;
+                    set_nxt = TWO;
+                end
+                else begin
+                    state_nxt = ALLOCATE;
+                    set_nxt = ONE;
+                end
+            end
+        end
+        READ: begin
+            //$display("read");
+            state_nxt = IDLE;
+            proc_stall_nxt = 0;
+            set_nxt = NONE;
+        end
+        WRITE: begin
+            //$display("write");
+            state_nxt = IDLE;
+            proc_stall_nxt = 0;
+            set_nxt = NONE;
+        end
+        ALLOCATE: begin
+            //$display("allocate");
+            proc_stall_nxt = 1;
+            set_nxt = set;
+            if (mem_ready) begin
+                if(proc_read && ~proc_write) begin
+                    state_nxt = READ;
+                end
+                else if (proc_write && ~proc_read) begin
+                    state_nxt = WRITE;
+                end
+            end
+            else begin
+                state_nxt = ALLOCATE;
+            end
+        end
+        WRITEBACK: begin
+            //$display("writeback");
+            proc_stall_nxt = 1;
+            set_nxt = set;
+            if (mem_ready) begin
+                state_nxt = ALLOCATE;
+            end
+            else begin
+                state_nxt = WRITEBACK;
+            end
+        end
+        default: begin
+            state_nxt = IDLE;
+            proc_stall_nxt = 0;
+            set_nxt = NONE;
+        end
+    endcase
+end
+
+always @(*) begin
+    // initial value
+    for(i=0;i<BLOCKNUM;i=i+1)begin
+        cch1_nxt[i] = cch1[i];
+        valid1_nxt[i] = valid1[i];
+        tag1_nxt[i] = tag1[i];
+        dirty1_nxt[i] = dirty1[i];
+        cch2_nxt[i] = cch2[i];
+        valid2_nxt[i] = valid2[i];
+        tag2_nxt[i] = tag2[i];
+        dirty2_nxt[i] = dirty2[i];
+    end
+    // proc_stall_nxt = 0;
+    proc_rdata_nxt = 0;
+    mem_read_nxt = 0;
+    mem_write_nxt = 0;
+    mem_addr_nxt = 0;
+    mem_wdata_nxt = 0;
+
+    case (state)
+        READ: begin
+            if (set == ONE) begin
+                case (word_idx)
+                    0: proc_rdata_nxt = cch1[block_now][31:0];
+                    1: proc_rdata_nxt = cch1[block_now][63:32];
+                    2: proc_rdata_nxt = cch1[block_now][95:64];
+                    3: proc_rdata_nxt = cch1[block_now][127:96];
+                    default: proc_rdata_nxt = 0; 
+                endcase
+            end
+            else if (set == TWO) begin
+                case (word_idx)
+                    0: proc_rdata_nxt = cch2[block_now][31:0];
+                    1: proc_rdata_nxt = cch2[block_now][63:32];
+                    2: proc_rdata_nxt = cch2[block_now][95:64];
+                    3: proc_rdata_nxt = cch2[block_now][127:96];
+                    default: proc_rdata_nxt = 0; 
+                endcase
+            end
+        end 
+        WRITE: begin
+            if (set == ONE) begin
+                case (word_idx)
+                    0: cch1_nxt[block_now][31:0] = proc_wdata;
+                    1: cch1_nxt[block_now][63:32] = proc_wdata;
+                    2: cch1_nxt[block_now][95:64] = proc_wdata;
+                    3: cch1_nxt[block_now][127:96] = proc_wdata;
+                    default: cch1_nxt[block_now] = cch1[block_now];
+                endcase
+                tag1_nxt[block_now] = tag_now;
+                dirty1_nxt[block_now] = 1;
+            end
+            else if (set == TWO) begin
+                case (word_idx)
+                    0: cch2_nxt[block_now][31:0] = proc_wdata;
+                    1: cch2_nxt[block_now][63:32] = proc_wdata;
+                    2: cch2_nxt[block_now][95:64] = proc_wdata;
+                    3: cch2_nxt[block_now][127:96] = proc_wdata;
+                    default: cch2_nxt[block_now] = cch2[block_now];
+                endcase
+                tag2_nxt[block_now] = tag_now;
+                dirty2_nxt[block_now] = 1;
+            end
+        end
+        ALLOCATE: begin
+            if (~mem_ready) begin
+                mem_read_nxt = 1;
+                mem_write_nxt = 0;
+                mem_wdata_nxt = 0;
+                mem_addr_nxt = proc_addr[29:2];
+            end
+            else begin
+                if (set == ONE) begin
+                    tag1_nxt[block_now] = tag_now;
+                    valid1_nxt[block_now] = 1;
+                    dirty1_nxt[block_now] = 0;
+                    cch1_nxt[block_now] = mem_rdata;
+                end
+                else if (set == TWO) begin
+                    tag2_nxt[block_now] = tag_now;
+                    valid2_nxt[block_now] = 1;
+                    dirty2_nxt[block_now] = 0;
+                    cch2_nxt[block_now] = mem_rdata;
+                end
+            end
+        end
+        WRITEBACK: begin
+            if (~mem_ready) begin
+                mem_write_nxt = 1;
+                if (set == ONE) begin
+                    mem_wdata_nxt = cch1[block_now];
+                    mem_addr_nxt = {tag1[block_now],block_now};
+                end
+                else if (set == TWO) begin
+                    mem_wdata_nxt = cch2[block_now];
+                    mem_addr_nxt = {tag2[block_now],block_now};
+                end
+            end
+        end
+        default: begin
+            // initial value
+            for(i=0;i<BLOCKNUM;i=i+1)
+                cch1_nxt[i] = cch1[i];
+                valid1_nxt[i] = valid1[i];
+                tag1_nxt[i] = tag1[i];
+                dirty1_nxt[i] = dirty1[i];
+                cch2_nxt[i] = cch2[i];
+                valid2_nxt[i] = valid2[i];
+                tag2_nxt[i] = tag2[i];
+                dirty2_nxt[i] = dirty2[i];
+            // proc_stall_nxt = 0;
+            proc_rdata_nxt = 0;
+            mem_read_nxt = 0;
+            mem_write_nxt = 0;
+            mem_addr_nxt = 0;
+            mem_wdata_nxt = 0;
+        end
+    endcase
+end
+
+//==== sequential circuit =================================
+always@( posedge clk ) begin
+    if( proc_reset ) begin
+        state <= IDLE;
+        set <= NONE;
+        for (i = 0; i<BLOCKNUM; i=i+1) begin
+            cch1[i]      <= 0;
+            tag1[i]      <= 0;
+            valid1[i]    <= 0;
+            dirty1[i]    <= 0;
+            cch2[i]      <= 0;
+            tag2[i]      <= 0;
+            valid2[i]    <= 0;
+            dirty2[i]    <= 0;
+        end
+        proc_stall      <= 0;
+        proc_rdata      <= 0;
+        mem_read        <= 0;
+        mem_write       <= 0;
+        mem_addr        <= 0;
+        mem_wdata       <= 0;
+    end
+    else begin
+        state <= state_nxt;
+        set <= set_nxt;
+        for (i = 0; i<BLOCKNUM; i=i+1) begin
+            cch1[i]      <= cch1_nxt[i];
+            tag1[i]      <= tag1_nxt[i];
+            valid1[i]    <= valid1_nxt[i];
+            dirty1[i]    <= dirty1_nxt[i];
+            cch2[i]      <= cch2_nxt[i];
+            tag2[i]      <= tag2_nxt[i];
+            valid2[i]    <= valid2_nxt[i];
+            dirty2[i]    <= dirty2_nxt[i];
+        end
+        proc_stall      <= proc_stall_nxt;
+        proc_rdata      <= proc_rdata_nxt;
+        mem_read        <= mem_read_nxt;
+        mem_write       <= mem_write_nxt;
+        mem_addr        <= mem_addr_nxt;
+        mem_wdata       <= mem_wdata_nxt;
+    end
 end
 
 endmodule
